90b6bb6a98557634e09c8e6fdb05b31e9b754678
Ryan Arnold Zoleta <ryanarnold.zoleta@gmail.com>
2020-04-09 11:05:34 +0800

    Remove unused code

--- classes.py
+++ classes.py
@@ -1,5 +1,3 @@
-from utils import strip_kana, strip_special_chars
-import re
 
 class Word(object):
     def __init__(self, kana, kanji, english, category, status):
@@ -8,20 +6,6 @@ class Word(object):
         self.english = english
         self.category = category
         self.status = status
-
-
-    def is_containing_kanjis_in_list(self, kanji_list):
-        pure_kanji = self.pure_kanji()
-
-        for k in pure_kanji:
-            if k not in kanji_list:
-                return False
-
-        return True
-
-
-    def pure_kanji(self):
-        return strip_special_chars(strip_kana(self.kanji))
 
 
 class Kanji(object):
